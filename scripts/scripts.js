a911279a0ff906d5b688d5a4b49f4bba40059c00
valkyrie-max <alisakitkina2016@gmail.com>
2020-04-15 17:06:32 -0500

    loaded script and jquery; working on code refacturing for the dropdown menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,37 @@
-// namespacing
+// // namespacing
 const app = {};
 
-// SHUFFLING ARRAY randomly
-// Fisher-Yates shuffle. 
-app.shuffle = function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    // console.log(app.sourceArray, `...remove log`); 
+// // SHUFFLING ARRAY randomly
+// // Fisher-Yates shuffle. 
+// app.shuffle = function shuffle(array) {
+//     for (let i = array.length - 1; i > 0; i--) {
+//         let j = Math.floor(Math.random() * (i + 1));
+//         [array[i], array[j]] = [array[j], array[i]];
+//     }
+//     // console.log(app.sourceArray, `...remove log`); 
+// }
+
+
+// once the select button is clicked => drop the menu down
+app.dropdownMenu = function() {
+    $(`button`).on(`click`, function() {
+        $(`#check`).addClass(`animated fadeIn`).one(`animationend`, function() {
+            $(this).removeClass(`animated fadeIn`)
+        })
+    })
 }
-
-
 
 
 // init FUNCTION Calls
 app.init = () => {
-    app.shuffle(app.sourceArray);  // JUST NEED TO supply array
+    // app.shuffle(app.sourceArray);  // JUST NEED TO supply array
     // app.nextFuction();
+    app.dropdownMenu();
 }
+
 
 // DOCUMENT READY... with init FUNCTION CALL
 $(() => {
     app.init();
+
 })
