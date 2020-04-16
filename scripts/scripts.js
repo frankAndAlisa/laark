7bb4b9be21f460e094c9e376c338ea6dd014cfbd
Frank Hoedl <frankhoedl@mac.com>
2020-04-16 17:04:40 -0400

    minor change, data-theme

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,21 @@
 // // namespacing
 const app = {};
 
-// // SHUFFLING ARRAY randomly
-// // Fisher-Yates shuffle. 
-// app.shuffle = function shuffle(array) {
-//     for (let i = array.length - 1; i > 0; i--) {
-//         let j = Math.floor(Math.random() * (i + 1));
-//         [array[i], array[j]] = [array[j], array[i]];
-//     }
-//     // console.log(app.sourceArray, `...remove log`); 
-// }
+// ajax key
+app.key = `fbeuXXM5`;
 
+// ajax call
+app.getArt = () => {
+    $.ajax({
+        url: `https://www.rijksmuseum.nl/api/en/collection`,
+        method: `GET`,
+        dataType: `json`,
+        data: {
+            key: app.key,
+            format: `json` 
+        }
+    })
+}
 
 // once the select button is clicked => menu is displayed
 app.dropdownMenu = function() {
@@ -23,6 +28,8 @@ app.dropdownMenu = function() {
         $(`.optionsBox`).addClass(`border slower animated fadeIn`)
     });
 }
+
+
 
 // init FUNCTION Calls
 app.init = () => {
@@ -37,3 +44,17 @@ $(() => {
     app.init();
 
 })
+
+
+// EXTRA CODE BITS
+// _______________________________________________
+// // SHUFFLING ARRAY randomly
+// // Fisher-Yates shuffle. 
+// app.shuffle = function shuffle(array) {
+//     for (let i = array.length - 1; i > 0; i--) {
+//         let j = Math.floor(Math.random() * (i + 1));
+//         [array[i], array[j]] = [array[j], array[i]];
+//     }
+//     // console.log(app.sourceArray, `...remove log`); 
+// }
+// -------------------------------------------------
